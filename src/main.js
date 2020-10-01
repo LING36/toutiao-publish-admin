3a5c329af6efc8e882bf9d697cec526a3cc17d5c
LING36 <1wling123@sina.cn>
2020-10-01 22:51:58 +0800

    scr

---
+++ src/main.js
@@ -0,0 +1,21 @@
+/*
+*项目的启动入口
+*/
+// 加载了vue
+import Vue from 'vue'
+// 加载了根组件
+import App from './App.vue'
+// 加载了路由
+import router from './router'
+
+// 关闭生产环境提示
+Vue.config.productionTip = false
+
+// 创建vue根实例
+// 把router 配置到根实例中
+// 通过render方法把app根组件渲染到#app入口节点
+new Vue({
+  router,
+  render: h => h(App)
+  // el: '#app'//等价于$mount('#app')
+}).$mount('#app')
