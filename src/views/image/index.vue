07860663bcd9355cfb873cdd6990067ed2f07167
LING36 <1wling123@sina.cn>
2020-10-07 23:14:58 +0800

    素材管理

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -23,8 +23,9 @@
                     class="upload-btn"
                     size='medium'
                     type="success"
+                    @click="dialogTableVisible = true"
                 >
-                上传素材
+                    上传素材
                 </el-button>
             </div>
             <div class="images-box" v-loading="loading">
@@ -51,7 +52,35 @@
                 >
             </el-pagination>
 
-       </el-card>
+        </el-card>
+
+       <!-- 弹层 -->
+        <el-dialog
+            title="上传素材"
+            :visible.sync="dialogTableVisible"
+            :append-to-body="true"
+            width="400px"
+            >
+            <!--
+                upload 组件本身就支持请求提交上传操作，说白了你使用它不用自己去发请求，它会自己发
+                请求方法：默认就是post
+                请求路径：action 必须写完整路径
+                请求头：headers
+            -->
+            <el-upload
+                class="upload-demo"
+                drag
+                action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+                :headers="uploadHeaders"
+                name ="image"
+                :on-success="onUploadSuccess"
+                v-if="dialogTableVisible"
+                multiple>
+                <i class="el-icon-upload"></i>
+                <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+        </el-dialog>
 
   </div>
 </template>
@@ -63,6 +92,7 @@ export default {
   props: {},
   components: {},
   data () {
+    const token = localStorage.getItem('token')
     return {
       images: [],
       loading: true,
@@ -70,6 +100,10 @@ export default {
         collect: false,
         page: 1, // 页码
         per_page: 18 // 一页条数
+      },
+      dialogTableVisible: false, // 控制弹层显示
+      uploadHeaders: {
+        Authorization: `Bearer ${token}`
       }
     }
   },
@@ -101,8 +135,18 @@ export default {
     typeChange () {
       this.params.page = 1
       this.loadImages()
-    }
+    },
 
+    // 素材图片上传成功
+    onUploadSuccess () {
+      this.params.collect = false
+      this.dialogTableVisible = false
+      this.$message({
+        message: '素材上传成功',
+        type: 'success'
+      })
+      this.loadImages()
+    }
   },
   computed: {},
   watch: {},
